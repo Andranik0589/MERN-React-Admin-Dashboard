0fdc6d97be2f3a28c9eb746a13931d2bf7762ee9
Andranik Khachatryan <andranik.khachatryan0589@gmail.com>
2023-02-03 18:48:00 +0300

    geography nivo ,handling server,MDB

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ import salesRoutes from './routes/sales.js';
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-import { dataUser, dataProduct, dataProductStat } from './data/index.js';
+import Transaction from './models/Transaction.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
 
 // CONFIGURATIONS
 dotenv.config();
@@ -46,7 +47,7 @@ mongoose
     /* ONLY ADD DATA ONE TIME */
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
-
+    //Transaction.insertMany(dataTransaction);
     //User.insertMany(dataUser);
   })
   .catch((error) => console.log(error));
